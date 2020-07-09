4085669c3e40d0a3dd26e1a1d60a6467404b7252
mckib2 <nicholas.bgp@gmail.com>
2020-07-08 19:16:02 -0700

    flake8-ify

--- pyHiGHS/tests/test_highs_wrapper.py
+++ pyHiGHS/tests/test_highs_wrapper.py
@@ -11,12 +11,12 @@ from scipy.sparse import csc_matrix
 
 from _highs.highs_wrapper import highs_wrapper
 
+
 class TestHighsWrapperCoverage(unittest.TestCase):
     '''
     Smoke tests to try to cover as many lines of code
     as possible.
     '''
-
 
     def setUp(self):
         '''Default values for simple testing'''
@@ -29,14 +29,13 @@ class TestHighsWrapperCoverage(unittest.TestCase):
         self.ub = np.zeros(self.n)
         self.options = {'message_level': 0}
 
-
     def test_yes_rows_cols(self):
         '''
         Test numrow > 0 branch and numcol > 0 branch.
         Also tests nonempty A.
         '''
-        assert(self.A.shape[0] > 0) # numrow > 0
-        assert(self.A.shape[1] > 0) # numcol > 0
+        assert(self.A.shape[0] > 0)  # numrow > 0
+        assert(self.A.shape[1] > 0)  # numcol > 0
         highs_wrapper(
             self.c,
             self.A.indptr,
@@ -48,14 +47,13 @@ class TestHighsWrapperCoverage(unittest.TestCase):
             self.ub,
             self.options)
 
-
     def test_no_rows(self):
         '''Test numrow == 0 branch'''
         A = csc_matrix(np.ones((0, self.n)))
         lhs, rhs = np.zeros(0), np.zeros(0)
-        assert(lhs.size == 0) # numrow == 0
-        assert(rhs.size == 0) # numrow == 0
-        assert(A.shape[0] == 0) # numrow == 0
+        assert(lhs.size == 0)  # numrow == 0
+        assert(rhs.size == 0)  # numrow == 0
+        assert(A.shape[0] == 0)  # numrow == 0
         highs_wrapper(
             self.c,
             A.indptr,
@@ -66,7 +64,6 @@ class TestHighsWrapperCoverage(unittest.TestCase):
             self.lb,
             self.ub,
             self.options)
-
 
     def test_no_cols(self):
         '''Test numcol == 0 branch'''
@@ -88,11 +85,10 @@ class TestHighsWrapperCoverage(unittest.TestCase):
             ub,
             self.options)
 
-
     def test_empty_A(self):
         '''Test nnz == 0 branch'''
         A = csc_matrix(np.zeros(self.A.shape))
-        assert(A.nnz == 0) # nnz == 0
+        assert(A.nnz == 0)  # nnz == 0
         highs_wrapper(
             self.c,
             A.indptr,
@@ -103,7 +99,6 @@ class TestHighsWrapperCoverage(unittest.TestCase):
             self.lb,
             self.ub,
             self.options)
-
 
     def test_message_level_NONE(self):
         '''Test message_level == NONE branch'''
@@ -118,7 +113,6 @@ class TestHighsWrapperCoverage(unittest.TestCase):
             self.ub,
             {'message_level': 0})
 
-
     def test_message_level_not_NONE(self):
         '''Test message_level != NONE branch'''
         highs_wrapper(
@@ -131,7 +125,6 @@ class TestHighsWrapperCoverage(unittest.TestCase):
             self.lb,
             self.ub,
             {'message_level': 1})
-
 
     def test_opt_warning_bool(self):
         '''Test opt_warning bool branch'''
@@ -147,7 +140,6 @@ class TestHighsWrapperCoverage(unittest.TestCase):
             self.ub,
             options={opt: np.pi})
 
-
     def test_opt_warning_bool2string(self):
         '''Test opt_warning bool to string branch'''
         opt = 'presolve'
@@ -161,7 +153,6 @@ class TestHighsWrapperCoverage(unittest.TestCase):
             self.lb,
             self.ub,
             {opt: np.pi})
-
 
     def test_opt_warning_int(self):
         '''Test opt_warning int branch'''
@@ -177,7 +168,6 @@ class TestHighsWrapperCoverage(unittest.TestCase):
             self.ub,
             {opt: np.pi})
 
-
     def test_opt_warning_double(self):
         '''Test opt_warning double branch'''
         opt = 'dual_feasibility_tolerance'
@@ -192,7 +182,6 @@ class TestHighsWrapperCoverage(unittest.TestCase):
             self.ub,
             {opt: "Not a double"})
 
-
     def test_opt_warning_string(self):
         '''Test opt_warning string branch'''
         opt = 'solver'
@@ -206,7 +195,6 @@ class TestHighsWrapperCoverage(unittest.TestCase):
             self.lb,
             self.ub,
             {opt: np.pi})
-
 
     def test_bad_solution(self):
         '''Test bad solution branch'''
@@ -226,7 +214,7 @@ class TestHighsWrapperCoverage(unittest.TestCase):
             lb,
             ub,
             self.options)
-        assert(res['fun'] is None) # bad solution
+        assert(res['fun'] is None)  # bad solution
 
 
 if __name__ == '__main__':
